7417b0d0dbc4cf1da395338016cb86fd462abf67
Gil Solis <gilsolisuw@gmail.com>
2021-07-12 14:54:51 -0700

    solution 7/21

--- dailystandup.js
+++ dailystandup.js
@@ -3,9 +3,17 @@
 
 // For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
 
-// Test.assertEquals(repeats([4,5,7,5,4,8]),15);
-// Test.assertEquals(repeats([9, 10, 19, 13, 19, 13]),19);
-// Test.assertEquals(repeats([16, 0, 11, 4, 8, 16, 0, 11]),12);
-// Test.assertEquals(repeats([5, 17, 18, 11, 13, 18, 11, 13]),22);
-// Test.assertEquals(repeats([5, 10, 19, 13, 10, 13]),24);
+// console.log(repeats([4,5,7,5,4,8]),15);
+// console.log(repeats([9, 10, 19, 13, 19, 13]),19);
+// console.log(repeats([16, 0, 11, 4, 8, 16, 0, 11]),12);
+// console.log(repeats([5, 17, 18, 11, 13, 18, 11, 13]),22);
+// console.log(repeats([5, 10, 19, 13, 10, 13]),24);
 
+const repeats = arr => arr.filter((num) => arr.indexOf(num) ===  arr.lastIndexOf(num)).reduce((a, b) => a + b, 0)
+console.log(repeats([4,5,7,5,4,8]),15);
+console.log(repeats([9, 10, 19, 13, 19, 13]),19);
+console.log(repeats([16, 0, 11, 4, 8, 16, 0, 11]),12);
+console.log(repeats([5, 17, 18, 11, 13, 18, 11, 13]),22);
+console.log(repeats([5, 10, 19, 13, 10, 13]),24);
+console.log(repeats([-1, 4, 3, 4, 5]))
+console.log(repeats([-1, 4, 4, -1, 5]))
