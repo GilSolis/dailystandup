299fbbc8470e8c265a7f43eba18ed41f668e14e2
Gil Solis <gilsolisuw@gmail.com>
2021-07-16 11:47:37 -0700

    daily problem solved

--- dailystandup.js
+++ dailystandup.js
@@ -65,10 +65,30 @@ function urlReplace(str){
 // You may assume the input only contain English alphabet and spaces.
 
 
-// ********solution*********
+// ********solution********* 
+// 
 function reverseCapAndOrder(str) {
   str = str.replace(/\s+/g, ' ')
   let capReverse = str.trim().split('').map(letter => letter === letter.toUpperCase() ? letter.toLowerCase() : letter.toUpperCase()).join('')
   return capReverse.split(' ').reverse().join(' ')
   
 }
+
+// Count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+// From: https://www.codewars.com/kata/52efefcbcdf57161d4000091
+
+// ********solution*********
+str = str.toLowerCase()
+  let charMap = {}
+  for (let char of str) {
+    if (charMap.hasOwnProperty(char)){
+      charMap[char]++
+    } else {
+      charMap[char] = 1
+    }
+  }
+  let sortedObj = Object.keys(charMap).sort().reduce((acc, key) => (acc[key] = charMap[key], acc), {})
+  return sortedObj
