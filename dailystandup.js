8b90a7703cb764cce91c3a999dbff8a78b133348
Gil Solis <gilsolisuw@gmail.com>
2021-07-17 21:07:45 -0700

    daily problem solved

--- dailystandup.js
+++ dailystandup.js
@@ -90,5 +90,34 @@ str = str.toLowerCase()
       charMap[char] = 1
     }
   }
-  let sortedObj = Object.keys(charMap).sort().reduce((acc, key) => (acc[key] = charMap[key], acc), {})
-  return sortedObj
+
+
+  // 7/17
+  // There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
+// ********solution*********
+function findUniq(arr){
+  return arr.filter(num => arr.indexOf(num) === arr.lastIndexOf(num))[0]
+}
+
+// console.log(findUniq([ 1, 1, 1, 2, 1, 1 ]), 2)
+// console.log(findUniq([ 0, 0, 0.55, 0, 0 ]), 0.55)
+
+
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+// Don't change the order of the elements that are left.
+// ********solution*********
+function removeSmallest(arr){
+  let arr2 = arr
+  let smallest = Math.min(...arr2)
+  let idx = arr2.indexOf(smallest)
+  return [...arr2.slice(0, idx), ...arr2.slice(idx + 1)]
+}
+
+// console.log(removeSmallest([1,2,3,4,5])) //[2,3,4,5]
+// console.log(removeSmallest([5,3,2,1,4])) //[5,3,2,4]
+// console.log(removeSmallest([2,2,1,2,1])) //[2,2,2,1]
