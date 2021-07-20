265ea2134d68fcdb3c8ea0ea074be6051bfd6d9c
Gil Solis <gilsolisuw@gmail.com>
2021-07-19 20:57:31 -0700

    problem and solution

--- dailystandup.js
+++ dailystandup.js
@@ -155,3 +155,31 @@ function high(x){
 }
 
 console.log(high('what time are we climbing up the volcano'), 'volcano')
+
+
+// 7/20
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+// Examples
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))((" 
+// Notes
+
+// Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+
+
+function duplicateEncode(word){
+  let encodeString = ""
+  word = word.toLowerCase()
+  word.split('').forEach((letter, i, arr) => {
+    if(word.split(letter).length === 2){
+      encodeString += '('
+    } else if (word.split(letter).length > 2){
+      encodeString += ')'
+    }
+  })
+
+  return encodeString 
+}
