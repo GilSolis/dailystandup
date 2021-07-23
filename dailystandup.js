3800cd035dc7502b8e1a60210ae3a02476821ac5
Gil Solis <gilsolisuw@gmail.com>
2021-07-22 17:21:33 -0700

    problem and solution

--- dailystandup.js
+++ dailystandup.js
@@ -215,3 +215,19 @@ function solve(arr){
   
   return answer
 };
+
+// 7/22
+// Given an array of numbers (in string format), you must return a string. The numbers correspond to the letters of the alphabet in reverse order: a=26, z=1 etc. You should also account for '!', '?' and ' ' that are represented by '27', '28' and '29' respectively.
+
+// All inputs will be valid.
+
+// Test.assertEquals(switcher(['24', '12', '23', '22', '4', '26', '9', '8']), 'codewars');
+// Test.assertEquals(switcher(['25','7','8','4','14','23','8','25','23','29','16','16','4']), 'btswmdsbd kkw'); 
+// Test.assertEquals(switcher(['4', '24']), 'wc'); 
+// ********solution*********
+function switcher(x){
+  const letters = ' zyxwvutsrqponmlkjihgfedcba!? '
+  //let str = ''
+  return x.reduce((acc, cv) => acc += letters.charAt(cv), '')
+  //
+  }
