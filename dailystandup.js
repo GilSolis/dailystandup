cd1147487a03904bd387300a9aac2cba00ba7ba8
Gil Solis <gilsolisuw@gmail.com>
2021-07-28 17:03:04 -0700

    problem and solution

--- dailystandup.js
+++ dailystandup.js
@@ -291,3 +291,19 @@ function sortArray(array) {
   let oddNums = array.filter(num => num%2).sort((a,b) => a-b)
   return array.map(num => num%2 ? oddNums.shift() : num)
 }
+
+// 7/28
+// The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age, oldest age].
+
+// The order of the numbers passed in could be any order. The array will always include at least 2 items. If there are two or more oldest age, then return both of them in array format.
+
+// For example:
+
+// twoOldestAges( [1, 2, 10, 8] ) // should return [8, 10]
+
+// *******solution********
+
+function twoOldestAges(ages){
+
+  return ages.sort((a,b) => a-b).slice(-2)
+}
