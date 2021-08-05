d9f1e247d1c0dd05228a61df3ef604c05cdc9059
Gil Solis <gilsolisuw@gmail.com>
2021-08-05 11:06:17 -0700

    problem & solution to 7/30

--- dailystandup.js
+++ dailystandup.js
@@ -350,3 +350,18 @@ var capitals = function (word) {
 function dup(arr) {
   return arr.map((word) => word.split('').filter((letter, i, array) => letter !== array[i-1]).join(''))
 }
+
+
+// 8/5
+// Your job is to figure out the index of which vowel is missing from a given string:
+
+// A has an index of 0,
+// E has an index of 1,
+// I has an index of 2,
+// O has an index of 3,
+// U has an index of 4.
+// Notes: There is no need for string validation and every sentence given will contain all vowles but one. Also, you won't need to worry about capitals.
+
+// Examples
+// "John Doe hs seven red pples under his bsket"          =>  0  ; missing: "a"
+// "Bb Smith sent us six neatly arranged range bicycles"  =>  3  ; missing: "o"
