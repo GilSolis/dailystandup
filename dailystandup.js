eacad24beb294e32dde35a8b0de30ab28297831b
Gil Solis <gilsolisuw@gmail.com>
2021-08-20 11:58:04 -0700

    update

--- dailystandup.js
+++ dailystandup.js
@@ -507,3 +507,19 @@ function findProfit(arr)  {
   }
   return profit
 }
+
+// 8/20
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+function majorityNum(arr){
+  const numObj = {}
+  let halfLength = arr.length/2
+
+  for (let num of arr){
+    numObj[num] = numObj[num] + 1 || 1
+    if(numObj[num] > halfLength) return num
+  }
+ 
+}
