66786ebc3f5474beb3332fe94b38f3e9058a0a7d
Gil Solis <gilsolisuw@gmail.com>
2021-08-21 14:30:06 -0700

    problem and solution

--- dailystandup.js
+++ dailystandup.js
@@ -513,7 +513,6 @@ function findProfit(arr)  {
 
 // The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
 
-
 function majorityNum(arr){
   const numObj = {}
   let halfLength = arr.length/2
