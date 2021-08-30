6dc013cac9ea851054bc5a6abe85e3b515d560d5
Gil Solis <gilsolisuw@gmail.com>
2021-08-30 11:49:58 -0700

    problem and solution

--- dailystandup.js
+++ dailystandup.js
@@ -581,8 +581,6 @@ function goodPairs(arr) {
   return count
 }
 
-
-
 // 8/24
 // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
