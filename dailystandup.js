9c640282ec1a9a6c84cfe251fb1b0735134a3f61
Gil Solis <gilsolisuw@gmail.com>
2021-09-17 11:56:46 -0600

    problem

--- dailystandup.js
+++ dailystandup.js
@@ -581,8 +581,6 @@ function goodPairs(arr) {
   return count
 }
 
-
-
 // 8/24
 // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
@@ -612,3 +610,24 @@ function dupsOrNah(arr){
 
   return false
 }
+
+// 8/26
+// You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+// Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+// Example 1:
+
+// Input: jewels = "aA", stones = "aAAbbbb"
+// Output: 3
+// Example 2:
+
+// Input: jewels = "z", stones = "ZZ"
+// Output: 0
+ 
+
+// Constraints:
+
+// 1 <= jewels.length, stones.length <= 50
+// jewels and stones consist of only English letters.
+// All the characters of jewels are unique.
