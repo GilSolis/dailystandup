cc175dc44f0301fe23665c905dec931b1c0865cb
Gil Solis <gilsolisuw@gmail.com>
2021-09-20 18:59:33 -0600

    problem

--- dailystandup.js
+++ dailystandup.js
@@ -692,3 +692,12 @@ function dupsOrNah(arr){
 // #Your task
 
 // You have to create the function factorial that receives n and returns n!. You have to use recursion.
+
+// 9/2
+// Find the greatest common divisor of two positive integers. The integers can be large, so you need to find a clever solution.
+
+// The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
+
+// Test.assertEquals(mygcd(30,12),6)
+// Test.assertEquals(mygcd(8,9),1)
+// Test.assertEquals(mygcd(1,1),1)
